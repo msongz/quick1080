0d3dcc0dbcb94415e7361e9aa888fc63771ec675
songz <songzmeng@gmail.com>
2018-01-30 22:36:48 +0800

    init release

---
+++ Quick 1080P.jsx
@@ -0,0 +1,49 @@
+//quick 1080
+//version 2.0
+//github.com/msongz
+//2018/01/30 21:42
+//WTFPL license
+{
+	function quick1080() {
+
+		app.beginUndoGroup("quick1080")
+
+		function addZero(num) {
+			return num < 10 
+			?"0" + num 
+			:num
+		}
+
+		var date = new Date(Date(0));
+		var day = date.getDate();
+		var mon = date.getMonth() + 1
+		var year = date.getFullYear();
+		var hour = date.getHours();
+		var min = date.getMinutes();
+		var sec = date.getSeconds();
+		var curDate = year + addZero(mon) + addZero(day) + addZero(hour) + addZero(min) + addZero(sec);
+
+		var compSet = {};
+		compSet.h = 1080;
+		compSet.w = 1920;
+		compSet.fps25 = 25;
+		compSet.fps60 = 60;
+		compSet.dur = 10;
+		compSet.layer = "";
+		compSet.pixel = 1;
+		compSet.name = "fuck-" + curDate;
+		compSet.layerColor = [0, 0, 0];
+		compSet.layerName = "" + curDate;
+		compSet.phName = "fuck the world mother fucker";
+
+		var comp = app.project.items.addComp(compSet.name, compSet.w, compSet.h, compSet.pixel, compSet.dur, compSet.fps60);
+		var layer = comp.layers.addSolid(compSet.layerColor, compSet.layerName, compSet.w, compSet.h, compSet.pixel, compSet.dur);
+		var layerSource = layer.source;
+			layerSource.setProxyWithPlaceholder(compSet.phName, 4, 4, 1, 1);
+
+		comp.openInViewer();
+
+		app.endUndoGroup();
+	}
+	quick1080()
+}
---
+++ README.md
@@ -0,0 +1,46 @@
+# quick 1080
+quickly create a 1080 composition with a full-fill placeholder inside
+
+![](http://ww1.sinaimg.cn/large/66e22e28ly1fnyztqvhdej20fx096wej.jpg)
+
+### Description
+
+  - no `GUI`
+
+  - compatible with `launch pad.jsx` `kbar` and `ft-toolbar`
+
+  
+### Usage & Installation
+
+  - put the jsx to :open_file_folder: `/ae installation folder/support file/script/`
+   
+
+
+  - run it from the <kbd>file</kbd>><kbd>scripts</kbd>><kbd>run script file</kbd>><kbd>quick1080.jsx</kbd> or any kind of tool bar
+
+### Compatibility
+
+  After Effects `CC+ or older`
+  
+  
+### Version
+
+ - 2.0 
+ 
+### License
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                    Version 2, January 2018
+
+ Copyright (C) 2018 songz meng <songzmeng@gmail.com>
+
+ Everyone is permitted to copy and distribute verbatim or modified
+ copies of this license document, and changing it is allowed as long
+ as the name is changed.
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
+
+
