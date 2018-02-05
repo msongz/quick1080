e27ecccdef5580bd3f37481f947006a1e93806b3
songz <songzmeng@gmail.com>
2018-02-06 00:46:16 +0800

    add alt key feature to switch fps

--- Quick 1080P.jsx
+++ Quick 1080P.jsx
@@ -26,7 +26,7 @@
 		var compSet = {};
 		compSet.h = 1080;
 		compSet.w = 1920;
-		compSet.fps25 = 25;
+		compSet.fps = ScriptUI.environment.keyboardState.altKey?25:60;
 		compSet.fps60 = 60;
 		compSet.dur = 10;
 		compSet.layer = "";
@@ -36,7 +36,7 @@
 		compSet.layerName = "" + curDate;
 		compSet.phName = "fuck the world mother fucker";
 
-		var comp = app.project.items.addComp(compSet.name, compSet.w, compSet.h, compSet.pixel, compSet.dur, compSet.fps60);
+		var comp = app.project.items.addComp(compSet.name, compSet.w, compSet.h, compSet.pixel, compSet.dur, compSet.fps);
 		var layer = comp.layers.addSolid(compSet.layerColor, compSet.layerName, compSet.w, compSet.h, compSet.pixel, compSet.dur);
 		var layerSource = layer.source;
 			layerSource.setProxyWithPlaceholder(compSet.phName, 4, 4, 1, 1);
